6db3403543ed02a9be4989fb6d4e436232177e39
Techsailor He <techsailor@163.com>
2014-08-24 08:28:10 +0800

    make ship moving

--- client/src/BackgroundLayer.js
+++ client/src/BackgroundLayer.js
@@ -20,6 +20,7 @@ var BackgroundLayer = cc.Layer.extend({
         //To show map at top-left of the window, the anchor should be set to top-left of the map
         this._backgroundMap.setPosition(cc.p(0,winSize.height));
         this._backgroundMap.setAnchorPoint(cc.p(0,1));
+
         this._mapLandLayer = this._backgroundMap.getLayer("LandLayer");
         this._mapLandLayer.setVisible(false);
         /*
@@ -35,8 +36,8 @@ var BackgroundLayer = cc.Layer.extend({
         cc.log("x = " + pos.x + " y = " + pos.y);
         //var playerStartX = this._backgroundLayer._mapPlayerStartPoint.x;
         //var playerStartY = this._backgroundLayer._mapPlayerStartPoint.y;
-        var playerStartX = 30;
-        var playerStartY = 600;
+        var playerStartX = 64;
+        var playerStartY = 384;
         this._playerSprite = new FloatingSprites();
         this._playerSprite.initWithFile(res.ship_png);
         this._playerSprite.setPosition(cc.p(playerStartX,playerStartY));
@@ -62,11 +63,43 @@ var BackgroundLayer = cc.Layer.extend({
                 var delta = cc.p(moveX,moveY);
                 //cannot use 'this', get target by tag
                 var playerSprite = event.getCurrentTarget().getChildByTag(1);
-                var newPos = cc.pAdd(delta,playerSprite.getPosition());
-                playerSprite.setPosition(newPos);
+                var viewLayer = event.getCurrentTarget();
+                var newPlayerPos = cc.pAdd(playerSprite.getPosition(),delta);
+                playerSprite.setPosition(newPlayerPos);
+                viewLayer.setViewPointCenter(newPlayerPos,delta);
+
+                event.getCurrentTarget().getTileCoordForPosition(newPlayerPos);
             }
         });
         cc.eventManager.addListener(listener,this);
-        //window.alert("add listener 1");
+
+    },
+    getTileCoordForPosition:function(pos){
+        var tileWidth = this._backgroundMap.getTileSize().width;
+        var tileHeight = this._backgroundMap.getTileSize().height;
+        var x = pos.x/tileWidth;
+        var y = (this._backgroundMap.getMapSize().height * tileHeight - cc.winSize.height +  pos.y)/tileHeight;
+
+        return cc.p(x,y);
+    },
+    getPositionForTileCoord:function(coord){
+        var tileWidth = this._backgroundMap.getTileSize().width;
+        var tileHeight = this._backgroundMap.getTileSize().height;
+        var x = coord.x * tileWidth + tileWidth/2;
+        var y = this._backgroundMap.getMapSize.height * tileHeight - coord.y * tileHeight - tileHeight/2;
+        return cc.p(x,y);
+    },
+    setViewPointCenter:function(newPlayerPos, delta) {
+        var winSize = cc.winSize;
+        var viewPos = this.getPosition();
+        //window.alert(newPlayerPos.x + " : " + newPlayerPos.y + " --- " + viewPos.x + " : " + viewPos.y  + " --- " + winSize.width + " : " + winSize.height);
+        if(newPlayerPos.x > winSize.width/2){
+           viewPos.x = viewPos.x - delta.x;
+        }
+        //Attention: top-left coord of player is (0, winSize.height)
+        if(newPlayerPos.y < winSize.height/2 ){
+           viewPos.y = viewPos.y - delta.y;
+        }
+        this.setPosition(viewPos);
     }
 })
